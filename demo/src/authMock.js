bb73586e6203d389294be70c66f163521e7bd9d7
James Harrington <=>
2019-10-22 15:42:46 -0400

    working demo

---
+++ demo/src/authMock.js
@@ -0,0 +1,126 @@
+/* eslint-disable no-new */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-console */
+import {
+  Authenticator,
+  UALError,
+  UALErrorType,
+  User
+} from 'universal-authenticator-library'
+const signatureResponse = {
+  wasBroadcast: true,
+  transactionId: 'Mock transaction Id'
+}
+
+function MockUser (accountName, chains) {
+  new User(chains)
+  this.accountName = ''
+  this.chains = []
+  this.accountName = accountName
+  this.chains = chains || [{ chainId: 0 }]
+}
+MockUser.prototype = Object.create(User.prototype)
+MockUser.prototype.constructor = MockUser
+
+MockUser.prototype.toString = function () {
+  return 'MockUser > ' + User.prototype.toString.call(this)
+}
+MockUser.prototype.getKeys = function () {
+  return Promise.resolve([])
+}
+MockUser.prototype.signTransaction = function (transaction, config) {
+  console.info('Requested signature config', config)
+  console.info('Requested signature for', transaction)
+  return Promise.resolve(signatureResponse)
+}
+MockUser.prototype.getAccountName = function () {
+  return Promise.resolve(this.accountName)
+}
+MockUser.prototype.getChainId = function () {
+  return Promise.resolve(this.chains[0].chainId)
+}
+MockUser.prototype.signArbitrary = function (publicKey, data, helpText) {
+  return new Promise((resolve, reject) => {
+    reject(
+      new UALError('Not implemented', UALErrorType.Signing, null, 'Mock User')
+    )
+  })
+}
+MockUser.prototype.verifyKeyOwnership = function (_) {
+  return new Promise(resolve => {
+    resolve(true)
+  })
+}
+
+function MockAuthenticator (chains) {
+  new Authenticator(chains)
+  this.loading = true
+}
+MockAuthenticator.prototype = Object.create(Authenticator.prototype)
+MockAuthenticator.prototype.constructor = MockAuthenticator
+
+MockAuthenticator.prototype.toString = function () {
+  return 'MockAuthenticator > ' + Authenticator.prototype.toString.call(this)
+}
+
+MockAuthenticator.prototype.getOnboardingLink = function () {
+  return 'https://localhost'
+}
+MockAuthenticator.prototype.getStyle = function () {
+  return {
+    /* tslint:disable */
+    icon: '',
+    /* tslint:enable */
+    textColor: 'white',
+    background: 'blue',
+    text: 'Mock Auths'
+  }
+}
+MockAuthenticator.prototype.shouldRender = function () {
+  return true
+}
+MockAuthenticator.prototype.shouldAutoLogin = function () {
+  return false
+}
+MockAuthenticator.prototype.login = function (accountName) {
+  // alert('Mock Login Authenticator Triggered');
+  console.info('Attempted login with ', accountName)
+
+  // Simulate login delay response
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve([new MockUser(accountName || '', this.chains)])
+    }, 1000)
+  })
+}
+MockAuthenticator.prototype.shouldRequestAccountName = function () {
+  return Promise.resolve(true)
+}
+MockAuthenticator.prototype.logout = function () {
+  console.info('Logging out')
+  return Promise.resolve()
+}
+MockAuthenticator.prototype.init = async function () {
+  this.loading = false
+}
+MockAuthenticator.prototype.isLoading = function () {
+  return this.loading
+}
+MockAuthenticator.prototype.isErrored = function () {
+  return false
+}
+MockAuthenticator.prototype.getError = function () {
+  return new UALError(
+    'Initialization Error',
+    UALErrorType.Initialization,
+    null,
+    'this guy'
+  )
+}
+MockAuthenticator.prototype.reset = function () {
+  console.info('resetting mock authenticator')
+}
+MockAuthenticator.prototype.requiresGetKeyConfirmation = function () {
+  return false
+}
+export { MockAuthenticator, MockUser }
--- demo/src/pages/Index.vue
+++ demo/src/pages/Index.vue
@@ -1,11 +1,49 @@
 <template>
   <q-page class="flex flex-center">
     <img alt="Quasar logo" src="~assets/quasar-logo-full.svg">
+    <ual-trigger :options="opts" @login="userCallback" />
   </q-page>
 </template>
 
 <script>
+import { MockAuthenticator } from '../authMock'
+import { Scatter } from 'ual-scatter'
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+  data () {
+    return {
+      user: {
+        name: '',
+        chainId: ''
+      },
+      opts: {
+        name: 'VUE UAL test',
+        nets: [{
+          chainId: 12345,
+          rpcEndpoints: [{
+            protocol: 'https',
+            host: 'example.net',
+            port: Number(443)
+          }]
+        }],
+        authenticators: [
+          { authenticator: Scatter, netChainIds: [12345], options: { appName: 'UAL Example' } },
+          MockAuthenticator
+        ]
+      }
+    }
+  },
+  methods: {
+    async userCallback (users) {
+      const loggedInUser = users[0]
+      this.user.name = await loggedInUser.getAccountName()
+      this.user.chainId = await loggedInUser.getChainId()
+      console.info('User Information:')
+      console.info('Account Name:', this.user.name)
+      console.info('Chain Id:', this.user.chainId)
+
+      // balanceUpdateInterval = setInterval(updateBalance, 1000)
+    }
+  }
 }
 </script>
